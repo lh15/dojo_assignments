2bf450ec9b5fea7f0707af4db04205ac60efee75
Leibelhecht <leibelhecht@gmail.com>
2017-06-07 14:42:30 -0500

    node.js math_module

---
+++ MEAN_stack/node.js/math_module/app.js
@@ -0,0 +1,15 @@
+var mathlib = require('./mathlib')();     //notice the extra invocation parentheses!
+console.log(mathlib);
+console.log(mathlib.add(1,2));
+console.log(mathlib.multiply(1,2));
+console.log(mathlib.square(4));
+console.log(mathlib.random(10,20));
+
+
+
+
+
+
+
+
+
